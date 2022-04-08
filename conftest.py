0113ef18e3a781cc75df86c0861931b992f28ccd
Shah rahman <toshah.rahman@gmail.com>
2022-04-08 15:47:03 +0300

    todo mvc practice fun 11

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright, expect
 
 @pytest.fixture
 def set_up(playwright: Playwright) -> None:
-    browser = playwright.firefox.launch(headless=True)
+    browser = playwright.webkit.launch(headless=True)
     context = browser.new_context()
     # Open new page
     page = context.new_page()
