b493cec5a0a0e91f7df276dc75b70b4e832e7744
Shah rahman <toshah.rahman@gmail.com>
2022-04-21 17:55:30 +0300

    todo mvc practice fun 43

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import Playwright
 
 @pytest.fixture(scope='function')
 def set_up(playwright: Playwright) -> None:
-    browser = playwright.webkit.launch(headless=False)
+    browser = playwright.webkit.launch(headless=True)
     sizes = [{'width': 375, 'height': 630}, {'width': 1024, 'height': 860}, {'width': 760, 'height': 630}]
     for size in sizes:
         context = browser.new_context(viewport=size)
