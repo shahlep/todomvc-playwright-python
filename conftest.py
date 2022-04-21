07cdd14e21ba01a2d6ef7c0183e5a988ed8d3baa
Shah rahman <toshah.rahman@gmail.com>
2022-04-21 06:57:51 +0300

    todo mvc practice fun 40

--- conftest.py
+++ conftest.py
@@ -4,11 +4,10 @@ from playwright.sync_api import Playwright
 
 @pytest.fixture(scope='function')
 def set_up(playwright: Playwright) -> None:
-    browser = playwright.webkit.launch(headless=True)
-    context = browser.new_context()
+    browser = playwright.webkit.launch(headless=False)
+    context = browser.new_context(viewport={'width': 375, 'height': 630})
     page = context.new_page()
     page.goto("https://demo.playwright.dev/todomvc/#/")
 
     return page
     page.close()
-
